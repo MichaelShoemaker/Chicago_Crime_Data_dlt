c1458c762f6018208a254f33897b66a852f9ed9b
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2025-07-24 12:32:21 -0500

    Added duckdb code

---
+++ 1-chicago_crime_duckdb/main.py
@@ -0,0 +1,36 @@
+import os
+import dlt
+import dotenv
+from dlt.sources.helpers import requests
+
+dotenv.load_dotenv()
+
+CHICAGO_CRIME_API = "https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=year=2025"
+
+CHI_APP_TOKEN = os.getenv("CHI_APP_TOKEN")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./bq_cred.json"
+
+@dlt.resource(name="chicago_crime", write_disposition="merge", primary_key="id")
+def fetch_crime_data():
+    headers = {
+        "X-App-Token": f"{CHI_APP_TOKEN}"
+    }
+    limit = 1000
+    offset = 0
+    while True:
+        url = f"https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=year=2025&$limit={limit}&$offset={offset}"
+        data = requests.get(url, headers=headers).json()
+        if not data:
+            break
+        yield data
+        offset += limit
+
+pipeline = dlt.pipeline(
+    pipeline_name="chicago_crime_pipeline",
+    destination="bigquery",
+    dataset_name="chicago_crime_data",
+    pipelines_dir="./myconfig"
+)
+
+load_info = pipeline.run(fetch_crime_data(), loader_file_format="jsonl")
+print(f"Loaded: {load_info}")
